0e94c19566eb266450e59ee839b88ea7ea08b2d5
ayushniroula9 <ayush@wiseadmit.io>
2023-09-04 23:42:30 +0545

    HugeImprovement

--- controllers/tableController.go
+++ controllers/tableController.go
@@ -1,29 +1,123 @@
-package controllers;
+package controllers
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"golang-restaurant-management/database"
+	"golang-restaurant-management/models"
+	"net/http"
+	"time"
 )
 
-func GetTables() gin.HandlerFunc{
-	return func(c *gin.Context){
+var tableCollection *mongo.Collection = database.OpenCollection(database.Client, "table")
+
+func GetTables() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		result, err := tableCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+		}
+		var allTables []bson.M
+		if err = result.All(ctx, &allTables); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+		}
+		c.JSON(http.StatusOK, allTables)
 
 	}
 }
 
-func GetTable() gin.HandlerFunc{
-	return func(c *gin.Context){
-
+func GetTable() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		tableId := c.Param("table_id")
+		var table models.Table
+		err := tableCollection.FindOne(ctx, bson.M{"table_id": tableId}).Decode(&table)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+		}
+		c.JSON(http.StatusOK, table)
 	}
 }
 
-func CreateTable() gin.HandlerFunc{
-	return func(c *gin.Context){
-
+func CreateTable() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var table models.Table
+		if err := c.BindJSON(&table); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		validationErr := validate.Struct(table)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": validationErr.Error(),
+			})
+			return
+		}
+		table.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		table.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		table.ID = primitive.NewObjectID()
+		table.Table_id = table.ID.Hex()
+		result, err := tableCollection.InsertOne(ctx, table)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.JSON(200, result)
 	}
 }
 
-func UpdateTable() gin.HandlerFunc{
-	return func(c *gin.Context){
-		
+func UpdateTable() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		tableId := c.Param("table_id")
+		var table models.Table
+		if err := c.BindJSON(&table); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		var updateObj primitive.D
+		if table.Number_of_guests != nil {
+			updateObj = append(updateObj, bson.E{"number_of_guests", table.Number_of_guests})
+		}
+		if table.Table_number != nil {
+			updateObj = append(updateObj, bson.E{"table_number", table.Table_number})
+		}
+
+		table.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		upsert := true
+		opt := options.UpdateOptions{
+			Upsert: &upsert,
+		}
+		filter := bson.M{"table_id": tableId}
+		result, err := tableCollection.UpdateOne(ctx, filter, bson.D{{"$set", updateObj}}, &opt)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.JSON(200, result)
 	}
 }
--- models/invoiceModel.go
+++ models/invoiceModel.go
@@ -7,11 +7,13 @@ import (
 )
 
 type Invoice struct {
-	ID             primitive.ObjectID `bson:"_id"`
-	Invoice_id     string             `json:"invoice_id"`
-	Order_id       string             `json:"order_id"`
-	Payment_method *string            `json:"payment_method" validate:"eq=CARD | ed=CASH | eq="`
-	Payment_status *string            `json:"payment_status" validate:"eq=PENDING | ed=PAID"`
-	Created_at     time.Time          `json:"created_at"`
-	Updated_at     time.Time          `json:"updated_at"`
+	ID               primitive.ObjectID `bson:"_id"`
+	Invoice_id       string             `json:"invoice_id"`
+	Invoice_due_date time.Time          `json:"invoice_due_date"`
+	Order_id         string             `json:"order_id"`
+	Payment_method   *string            `json:"payment_method" validate:"eq=CARD | ed=CASH | eq="`
+	Payment_status   *string            `json:"payment_status" validate:"eq=PENDING | ed=PAID"`
+	Created_at       time.Time          `json:"created_at"`
+	Updated_at       time.Time          `json:"updated_at"`
+	Payment_due_date time.Time          `json:"payment_due_date"`
 }
